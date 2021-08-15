158f535034843185e5090cc37d4bc3bdd7cfd186
Victor Marques <vhm.marques@hotmail.com>
2021-08-15 20:36:15 -0300

    Criando o projeto

---
+++ CasaDoSaber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CasaDoSaber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
